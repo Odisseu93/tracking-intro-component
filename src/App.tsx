047bc32deb557c6bcf3b406b91572d9e2d201536
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-05-04 21:26:14 -0300

    first basic HTML structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Product
+        Features
+        Pricing
+        Login
+
+        New
+        Monograph Dashboard
+
+        Powerful insights into your team
+        Project planning and time tracking for agile teams
+
+        Schedule a demo
+        to see a live preview
     </div>
   );
 }
